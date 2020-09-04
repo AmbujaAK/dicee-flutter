c62415e5f1eec2f24866c04abc91a383c4a00c5e
ekmbuma <ambuj.a.kumar@ericsson.com>
2020-09-05 01:42:45 +0530

    random dice generated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -24,6 +25,13 @@ class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
 
+  void generateRandomNumber(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -33,9 +41,8 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: (){
                 setState(() {
-                  leftDiceNumber = 5;
+                  generateRandomNumber();
                 });
-                print('L pressed! : $leftDiceNumber');
               },
               child: Image.asset('images/dice$leftDiceNumber.png'),
             ),
@@ -44,13 +51,12 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: (){
                 setState(() {
-                  rightDiceNumber = 3;
+                  generateRandomNumber();
                 });
-                print('L pressed! : $rightDiceNumber');
               },
               child: Image.asset('images/dice$rightDiceNumber.png'),
             ),
-          )
+          ),
         ],
       ),
     );
